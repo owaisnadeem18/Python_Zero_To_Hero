cd1ccb6e461a06072708512d6bb67b2ab106023a
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2023-10-03 23:37:20 +0500

    Add files via upload

---
+++ Day - 21/Day_21-python.py
@@ -0,0 +1,16 @@
+# Day : 21
+# 100 Days of Code
+# Topic : Functions Arguments in Python
+
+# X---------------------------------------------------------------------------------X
+
+def multi(a, b):
+    c = a*b
+    print(c)
+
+
+multi(8, 3)
+multi(8, 3)
+multi(8, 3)
+multi(8, 3)
+multi(8, 3)
---
+++ Day - 22/Day-22_python.py
@@ -0,0 +1,38 @@
+# # Day : 22
+# # 100 Days of Code
+# # Topic : Lists in Python
+
+# # X---------------------------------------------------------------------------------X
+
+# list = [8, 87, 96, 76, 34, True, "Owais"]
+
+# # print(type(list[0]))
+# # print(type(list[1]))
+# # print(type(list[2]))
+# # print(type(list[3]))
+# # print(type(list[4]))
+
+# print((list[0]))
+# print((list[1]))
+# print((list[2]))
+# print((list[3]))
+# print((list[4]))
+
+# print(list)
+# print(len(list))
+
+
+# if (95) in list:
+#     print("Yes")
+# else:
+#     print("No")
+
+# # list indexing
+
+# print(list[0:3])
+# print(list[5:7])
+# print(list[-5:6])
+
+# z = [i*i for i in range(6) if i % 2 == 0]
+
+# print(z)
