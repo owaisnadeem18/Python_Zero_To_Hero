b68f586e831113826ac5bba56dcb3c3b556bc869
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2023-10-05 23:59:55 +0500

    Add files via upload

---
+++ Day - 29/Day_29_Python.py
@@ -0,0 +1,13 @@
+def square(n):
+    '''This is a Doc String in Python language 
+    '''
+    print(f"The square of the {n} is =", n**2)
+
+
+square(4)
+square(7)
+square(14)
+square(12)
+square(15)
+
+print(square.__doc__)
